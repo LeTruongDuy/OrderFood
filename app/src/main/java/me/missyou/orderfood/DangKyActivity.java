f9f643a830350296da534090e949525f18613e9a
LTDuy <letruongduyy@gmail.com>
2017-09-28 09:08:41 +0700

    Hoàn thành giao diện đăng kí

--- app/src/main/java/me/missyou/orderfood/DangKyActivity.java
+++ app/src/main/java/me/missyou/orderfood/DangKyActivity.java
@@ -3,11 +3,11 @@ package me.missyou.orderfood;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class DangKyActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.layout_dangky);
     }
 }
